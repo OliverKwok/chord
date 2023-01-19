a98db52cc8b9a57c4d1eb5c56cdbe9fa6bb6c3bc
OliverKwok <oliver.kwok@vicosys.com.hk>
2023-01-19 17:32:32 +0800

    try sms

---
+++ backend/src/sms/sms.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateSmDto } from './dto/create-sm.dto';
+import { UpdateSmDto } from './dto/update-sm.dto';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const verifySid = process.env.TWILIO_VERIFY_SID;
+const client = require('twilio')(accountSid, authToken);
+
+@Injectable()
+export class SmsService {
+  create(createSmDto: CreateSmDto) {
+    return 'This action adds a new sm';
+  }
+
+  findAll() {
+    return `This action returns all sms`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} sm`;
+  }
+
+  update(id: number, updateSmDto: UpdateSmDto) {
+    return `This action updates a #${id} sm`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} sm`;
+  }
+}
