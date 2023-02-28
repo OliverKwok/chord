9bab83137d30ff0736f7a82b3fbf3f2694a834f3
OliverKwok <oliver.kwok@vicosys.com.hk>
2023-02-28 16:02:22 +0800

    tab style donw

--- printIos/App.tsx
+++ printIos/App.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faPrint} from '@fortawesome/free-solid-svg-icons';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
 
 import Print from './src/screen/Print';
 import Pos from './src/screen/Pos';
@@ -13,10 +18,58 @@ export default function App() {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {height: 100, backgroundColor: '#f1f1f1'},
         }}>
-        <Tab.Screen name="Print" component={Print} />
-        <Tab.Screen name="Pos" component={Pos} />
+        <Tab.Screen
+          name="Print"
+          component={Print}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={styles.tabContainer}>
+                <View style={{marginBottom: 4}}>
+                  <FontAwesomeIcon
+                    icon={faPrint}
+                    size={30}
+                    color={focused ? '#0d3b66' : '#666666'}
+                  />
+                </View>
+                <Text style={[styles.tabText, focused && {fontWeight: 'bold'}]}>
+                  Print
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Pos"
+          component={Pos}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={styles.tabContainer}>
+                <View style={{marginBottom: 4}}>
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    size={30}
+                    color={focused ? '#0d3b66' : '#666666'}
+                  />
+                </View>
+                <Text style={[styles.tabText, focused && {fontWeight: 'bold'}]}>
+                  Pos
+                </Text>
+              </View>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  tabContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabText: {color: '#666666', textTransform: 'uppercase', fontSize: 12},
+});
