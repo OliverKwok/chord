1811aac576799845605c06d55e63e437c9cd35aa
OliverKwok <oliver.kwok@vicosys.com.hk>
2023-02-28 15:38:04 +0800

    fontawesome icon works

--- printIos/src/screen/Pos.tsx
+++ printIos/src/screen/Pos.tsx
@@ -1,10 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faMugSaucer} from '@fortawesome/free-solid-svg-icons';
+
 const Pos = () => {
   return (
     <View style={styles.container}>
       <Text>Coming Soon</Text>
+      <FontAwesomeIcon icon={faMugSaucer} size={50} />
     </View>
   );
 };
